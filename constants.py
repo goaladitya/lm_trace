4e529f041c03f4e43346afbe8d5f4191c26d6c97
goaladitya <94adityagoyal@gmail.com>
2019-02-13 17:12:36 +0530

    Initial

---
+++ constants.py
@@ -0,0 +1,13 @@
+host = 'localhost'
+
+queue_name = 'dura1'
+publish_queue_name = 'dura2'
+
+routing_key = 'error'
+publish_routing_key = 'publish'
+
+exchange = 'direct-logs'
+exchange_type = 'direct'
+
+s3_bucket = ''
+s3_key = ''
